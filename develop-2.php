b6a65f70415494746b060438d36afe0acdc4b3bb
mercriumkappa <kappa@mercrium.com>
2015-02-10 16:22:38 +0900

    git commit --ammend can change the comment of this commit

---
+++ develop-2.php
@@ -0,0 +1,16 @@
+<?php
+
+$test-develop-2;
+
+class thisProject {
+	
+	static function staticFunc(){
+	}
+	
+	public function testFunc(){
+	}
+	
+	public function testFunc2(){
+		$this = $data;	
+	}
+}
---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+$developfile;
+
+class thisProject {
+	
+	static function staticFunc(){
+	}
+	
+	public function testFunc(){
+	}
+	
+	public function testFunc2(){
+		$this = $data;	
+	}
+}
